3b2db093e4d28a73474ab612b77ebcad042e7288
Shallom Alanemech <shallom.alanemech@gmail.com>
2023-04-26 18:42:09 -0400

    edited alternative names

--- Jupiter-website-ecs/main.tf
+++ Jupiter-website-ecs/main.tf
@@ -55,26 +55,26 @@ module "acm" {
 
 # create application load balancer
 module "alb" {
-  source = "../Modules/alb"
-  project_name = module.vpc.project_name
+  source                = "../Modules/alb"
+  project_name          = module.vpc.project_name
   alb_security_group_id = module.security-groups.alb_security_group_id
-  public_subnet_az1_id = module.vpc.public_subnet_az1_id
-  public_subnet_az2_id = module.vpc.public_subnet_az2_id
-  vpc_id = module.vpc.vpc_id
-  certificate_arn = module.acm.certificate_arn
+  public_subnet_az1_id  = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id  = module.vpc.public_subnet_az2_id
+  vpc_id                = module.vpc.vpc_id
+  certificate_arn       = module.acm.certificate_arn
 }
 
 # create ecs service
 module "ecs" {
-  source = "../Modules/ecs"
-  project_name = module.vpc.project_name
+  source                       = "../Modules/ecs"
+  project_name                 = module.vpc.project_name
   ecs-tasks-execution-role-arn = module.ecs_tasks_execution_role.ecs-tasks-execution-role-arn
-  container_image = var.container_image
-  region = module.vpc.region
-  private_app_subnet_az1_id = module.vpc.private_app_subnet_az1_id
-  private_app_subnet_az2_id = module.vpc.private_app_subnet_az2_id
-  ecs_security_group_id = module.security-groups.ecs_security_group_id
-  alb_target_group_arn = module.alb.alb_target_group_arn
+  container_image              = var.container_image
+  region                       = module.vpc.region
+  private_app_subnet_az1_id    = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id    = module.vpc.private_app_subnet_az2_id
+  ecs_security_group_id        = module.security-groups.ecs_security_group_id
+  alb_target_group_arn         = module.alb.alb_target_group_arn
 }
 
 
